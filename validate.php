9a82b50650ff2b53d7a8c5de31231a12710de437
Brad Hart <brad@tenacial.com>
2017-07-11 13:16:23 -0400

    skip end of line comments in java,js and php

--- validate.php
+++ validate.php
@@ -34,6 +34,7 @@ foreach ($rii as $file) {
 }
 foreach ($files as $filename) {
   $stripFN =  str_replace($src, ".", $filename); 
+  $fileExt = pathinfo($filename, PATHINFO_EXTENSION);
   $htmlOut = "";
   $appDepHTML = "";
   $xpathPrint = 0;
@@ -46,15 +47,14 @@ foreach ($files as $filename) {
        if(preg_match("/$searchRegex/i", $line)) {
           // strip whitespace off of beginning of line
           $line = trim($line);
-          // skip if # or //  or look for  //skipXpath to intentionally skip
-          if(preg_match("/^#/", $line) || preg_match("/^\/\//", $line) || preg_match("/\/\/skipXpath/", $line)) {
+          // skip if begin with (# or //) or contains /// or look for  //skipXpath to intentionally skip
+          if(preg_match("/^#/", $line) || preg_match("/^\/\//", $line) || preg_match("/\/\/skipXpath/", $line) || preg_match("/\/\/\//", $line)) {
              continue;
           }
           // if using regex to search for "//" - skip if matched http:// or https://
           if(preg_match("/http:\/\//", $line) || preg_match("/https:\/\//", $line)) {
              continue;
           }
-          $xpathPrint = 1;
           $xPathSplit = preg_split("/$searchRegex/i", $line);
           $xPathSplit[1] = strstr($xPathSplit[1], '.click', true) ?: $xPathSplit[1];
           $xPathSplit[1] = strstr($xPathSplit[1], '.sendKeys', true) ?: $xPathSplit[1];
@@ -67,7 +67,15 @@ foreach ($files as $filename) {
           if (strcmp($searchRegex, "\/\/") == 0){
              // if using regex to search for "//", add back to the beginning of the string
              $xPathSplit[1] = "//" . $xPathSplit[1];
+             // Noise reduction - try to eliminate processing comments at the end of lines using // in Java, JS, PHP
+             if(preg_match('(java|js|php)', $fileExt) === 1) { 
+                if(preg_match('(\"\(\/\/|\'\(\/\/|\"\/\/|\'\/\/|\.\/\/|\*\/\/)', $line) === 0){  //Xpath must match "(// , '(// , "// , '// , .// , *//
+                   //print "skipping: $line\n";
+                   continue;
+                }
+             } 
           }
+          $xpathPrint = 1;
           $xpScore = testXPath($urlValidate, $xPathSplit[1]);
           $labelStat = calcLabel($xpScore);
           $xPathEncode = "<a href=\"$urlValidateTarget" . urlencode($xPathSplit[1]) . "\">(View Details)</a> (line: $lineNum)";
